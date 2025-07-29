09eb5e0b763c6817da24054236f283065c1123e3
Shubham Sharma <shubhamsharma25501@gmail.com>
2025-07-29 20:40:51 +0530

    Moved Tic Tac Toe.html to Frontend and table.java to Backend

---
+++ Backend/table.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class table{
+public static void main(String args[]){
+Scanner obj=new Scanner(System.in);
+/*int n1;
+System.out.printf("Enter any Number : ");
+n1=obj.nextInt();
+System.out.println("Table of " +n1+ " is : ");
+for(int n=1;n<=10;n++)
+{
+System.out.println(n1+ " * " +n+ " = " +n1*n);
+}*/
+float n1;
+System.out.printf("Enter any Number : ");
+n1=obj.nextFloat();
+System.out.println("Table of " +n1+ " is : ");
+for(int n=1;n<=10;n++)
+{
+System.out.println(n1+ " * " +n+ " = " +n1*n);
+}
+}
+};
