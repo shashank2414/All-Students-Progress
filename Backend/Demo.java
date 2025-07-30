d8c77785c12ee5a9c6f80d2e16fd03c144cf4a68
shashank2414 <shashankdevindia@gmail.com>
2025-07-30 07:45:19 +0530

    First commit into Java Full Stack

---
+++ Backend/Demo.java
@@ -0,0 +1,6 @@
+class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello, Infosys Intern!");
+        System.out.println("Welcome to the world of Java Full Stack Development.");
+    }
+}
